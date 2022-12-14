8d85079dc69f9d39568532e6421f990d2893b9b2
santhiyasanthiya <santhiya30032@gmail.com>
2022-12-14 14:23:39 +0530

    pricecart

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.css"
 
 function App() {
-  let priceCard = [
+  let priceCard= [
     {
       title: "Free",
       price: 0,
@@ -126,7 +126,7 @@ function App() {
             
         },
         {
-            name: "Unlimited Free Subdomain ",
+            name:[<strong>Unlimited</strong>," ", "Free Subdomain"],
             isEnable : true
         },
         {
